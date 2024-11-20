7f69b8f6dbe2767394faf25395a538c373bc0deb
DomuStyle <oxgameboyxo@googlemail.com>
2024-11-20 15:59:20 +0100

    added: function: showQuestion()

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let currentQuestion = 0;
+
 function init() {
     document.getElementById('total_questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    
+    document.getElementById('question_text').innerHTML = question['question'];
 }
